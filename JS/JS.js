5d7daf74a6cb37386d889861e3c7dd784765222e
Hugh Grierson <hugh672@hotmail.co.uk>
2018-04-10 17:10:24 +0100

    latest

--- JS/JS.js
+++ JS/JS.js
@@ -1,3 +1,14 @@
 var y = "This is a variable stored in a JS file which will de displayed as an alert and to the console";
 window.alert(y);
 console.log(y);
+
+function square(x1) {
+    return x1 * x1;
+}
+console.log(square(2));
+
+function sum(s1,s2,s3) {
+    return s1 + s2 + s3;
+}
+console.log(sum(1,2,3));
+
